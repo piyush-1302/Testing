a79a19587eb2767cc3225271ed289e47d6155269
Piyush Kumar <30867212+piyush0901@users.noreply.github.com>
2018-02-24 20:12:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+ consumer_key:         'BCFPQ1Ia0EoGp7wd2TJWopEVb',
+  consumer_secret:      'DY59D9GSYcEhFqI6O0cqam9nBxUumpbNCWMxbG70jxzFM1WD1l',
+  access_token:         '571534969-TVN0lZ6MqGbxgT7MqqJn6puoX6yirOjSlgmH6yep',
+  access_token_secret:  'aJfcZwWVIhHtaQWhyoKEsFBbiAI5L8suioCDmCgwIeGqt'
+}
